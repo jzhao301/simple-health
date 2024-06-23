ddcad9678f43cc5a11319e28d27b953f9f9761a7
Jason Zhao <jzhao301@gmail.com>
2024-06-23 12:50:01 -0500

    multithread fuckery

--- llmClassifier.py
+++ llmClassifier.py
@@ -1,4 +1,4 @@
-import lamini
+import threading
 import os
 import json
 import openai
@@ -8,12 +8,15 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+OPENAI_API_KEYS = os.getenv("OPENAI_API_KEYS").split(',')
+print(OPENAI_API_KEYS[0])
+
 class DiseaseClassifier:
-  def __init__(self, disease):
+  def __init__(self, disease, api_key=None):
     self.disease_file = disease+'.json'
     with open(self.disease_file, "r") as fr:
       self.labels = json.load(fr)
-    self.client = openai.Client()
+    self.client = openai.Client(api_key=api_key)
 
   def classify(self, msg):
     def response(prompt):
@@ -21,7 +24,8 @@ class DiseaseClassifier:
                         model="gpt-3.5-turbo",
                         messages=[
                             {"role": "system", "content": f"You are a knowledgable assistant, an expert in clinical trials and know important symptoms of diseases.\n\nGiven dictionary of labels: {self.labels} \n and a prompt consisting of a set of patient notes, classify the message and return the label, confidence score, and a one sentence explanation in {{\"label\":__,\"confidence\":__,\"explaination\":__}} json format."},
-                            {"role": "user", "content": prompt},]
+                            {"role": "user", "content": prompt},],
+                        seed=42
                         ).choices[0].message.content)
       res['score'] = np.where(res['label'] == 'Negative', np.around(1.0-res['confidence'], 2), np.around(res['confidence'], 2))
       return res
@@ -29,19 +33,42 @@ class DiseaseClassifier:
     return ufun_response(msg)
   
 class BatchDiseaseClassifier:
-  def __init__(self, dc: DiseaseClassifier):
-    self.dc = dc
-    self.data = None
+  def __init__(self, disease, data_file, api_keys):
+    self.df = None
+    self.load_df(data_file)
+    self.disease = disease
+    self.threads = [threading.Thread(target=self.worker(api_key)) for api_key in api_keys]
 
-  def load(self, data_file):
-    self.data = pd.read_csv(data_file)
+  def load_df(self, data_file):
+    self.df = pd.read_csv(data_file)
+    self.df['Semaphore'] = 0
+    self.df['label'] = None
+    self.df['confidence'] = None
+    self.df['explanation'] = None
+    self.df['score'] = None
 
-  def loads(self, df):
-    self.data = df
+  def worker(self, api_key):
+    dc = DiseaseClassifier(self.disease, api_key)
+    while self.df['label'].count() < self.df['label'].size:
+      for i, row in self.df.iterrows():
+        if row['Semaphore'] == 0:
+          self.df.at[i, 'Semaphore'] = 1
+          self.df.loc[i, ['label', 'confidence', 'explanation', 'score']] = dc.classify(row['Patient Notes'])
+          # self.df.loc[i, 'score'] = np.where(self.df.loc[i, 'label'] == 'Negative', np.around(1.0-self.df.loc[i, 'confidence'], 2), np.around(self.df.loc[i, 'confidence'], 2))
+          print(self.df.loc[i])
+          self.df.at[i, 'Semaphore'] = 0
 
-  def iterate(self):
-    for msg in self.data:
-      yield self.dc.classify(msg['Patient Notes'])
+  def classify(self):
+    for thread in self.threads:
+      thread.start()
+
+    for thread in self.threads:
+      thread.join()
+    return self.df
+  
+bdc = BatchDiseaseClassifier('lymphedema', 'patient_data.csv', np.repeat(OPENAI_API_KEYS,10))
+# bdc.load_df()
+print(bdc.classify())
 
 # DISEASE_FILE = 'lymphedema.json'
 
